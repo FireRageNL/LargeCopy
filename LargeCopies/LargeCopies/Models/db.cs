8fccaf28988db731bd66c7fbef6f9fc3a6730421
FireRageNL <wc.eendje@gmail.com>
2016-06-02 09:52:42 +0200

    added Oracle managedDataAccess nuget package

---
+++ LargeCopies/LargeCopies/Models/db.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Oracle.ManagedDataAccess.Client;
+
+namespace LargeCopies.Models
+{
+    public class db
+    {
+        private readonly OracleConnection _conn = new OracleConnection();
+
+        public db()
+        {
+            _conn.ConnectionString = "Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = fhictora01.fhict.local)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = fhictora))); User ID = dbi347373; PASSWORD = Testpassword1234";
+        }
+    }
+}
