89610cc111e212cd724dc850b7e0bb38be6590a2
Roy v.Oldenbeek <roy_v_oldenbeek@hotmail.com>
2016-06-06 20:10:51 +0200

    added inheritance and started with db connections

---
+++ LargeCopies/LargeCopies/Models/Userdb.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using Microsoft.Ajax.Utilities;
+using Oracle.ManagedDataAccess.Client;
+
+namespace LargeCopies.Models
+{
+    public class Userdb : db
+    {
+        public bool Reguser(RegisterViewModel model)
+        {
+            try
+            {
+                conn.Open();
+                if (!model.NameConnector.IsNullOrWhiteSpace())
+                {
+                    cmd.CommandText =
+                        "INSERT INTO GEBRUIKER(VOORNAAM,TUSSENVOEGSEL,ACHTERNAAM,EMAIL,WACHTWOORD,GESLACHT,TELEFOONUMMER) VALUES(:vn,:tv,:an,:em,:ww,:gs,:tn)";
+                    cmd.Parameters.Add("vn", model.FirstName);
+                    cmd.Parameters.Add("tv", model.NameConnector);
+                    cmd.Parameters.Add("an", model.LastName);
+                    cmd.Parameters.Add("em", model.Email);
+                    cmd.Parameters.Add("ww", model.Password);
+                    cmd.Parameters.Add("gs", model.Sex);
+                    cmd.Parameters.Add("tn", model.Telephone);
+                }
+                else
+                {
+                    cmd.CommandText =
+                        "INSERT INTO GEBRUIKER(VOORNAAM,ACHTERNAAM,EMAIL,WACHTWOORD,GESLACHT,TELEFOONUMMER) VALUES(:vn,:an,:em,:ww,:gs,:tn)";
+                    cmd.Parameters.Add("vn", model.FirstName);
+                    cmd.Parameters.Add("an", model.LastName);
+                    cmd.Parameters.Add("em", model.Email);
+                    cmd.Parameters.Add("ww", model.Password);
+                    cmd.Parameters.Add("gs", model.Sex);
+                    cmd.Parameters.Add("tn", model.Telephone);
+                }
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return true;
+            }
+            catch (OracleException e)
+            {
+                Console.WriteLine("Oh noes error: " + e.Message);
+                conn.Close();
+                return false;
+            }
+        }
+    }
+}
--- LargeCopies/LargeCopies/Models/db.cs
+++ LargeCopies/LargeCopies/Models/db.cs
@@ -8,48 +8,22 @@ namespace LargeCopies.Models
 {
     public class db
     {
-        private readonly OracleConnection _conn = new OracleConnection();
+        public OracleConnection conn;
+        public OracleCommand cmd;
 
-        public db()
+        protected db()
         {
-            _conn.ConnectionString = "Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = fhictora01.fhict.local)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = fhictora))); User ID = dbi347373; PASSWORD = Testpassword1234";
-        }
 
-        public bool dbtest()
-        {
-            try
+            conn = new OracleConnection
             {
-                _conn.Open();
-                OracleCommand cmd = new OracleCommand
-                {
-                    BindByName = true,
-                    Connection = _conn,
-                    CommandText = "SELECT COUNT(*) FROM ADRES"
-                };
-                OracleDataReader dr = cmd.ExecuteReader();
-                int count = dr.GetInt32(0);
-                if(count > 0)
-                {
-                    _conn.Close();
-                    return true;
-                }
-                else
-                {
-                    _conn.Close();
-                    return false;
-                }
-            }
-            catch (OracleException e)
+                ConnectionString =
+                    "Data Source = (DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = fhictora01.fhict.local)(PORT = 1521)))(CONNECT_DATA = (SERVER = DEDICATED)(SERVICE_NAME = fhictora))); User ID = dbi347373; PASSWORD = Testpassword1234"
+            };
+            cmd = new OracleCommand
             {
-                Console.WriteLine("Oracle error: " + e.Message);
-                _conn.Close();
-                return false;
-            }
-        }
-
-        public bool register(RegisterViewModel model)
-        {
-            throw new NotImplementedException();
+                BindByName = true,
+                Connection = conn,
+            };
         }
     }
 }
