886167b7330d2a4436e9ce05e61a8e92869feeba
Roy v.Oldenbeek <roy_v_oldenbeek@hotmail.com>
2016-06-16 22:33:49 +0200

    Added login + admin level unit tests

---
+++ LargeCopies/LargeCopies.test/LoginTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LargeCopies;
+using LargeCopies.Models;
+
+namespace LargeCopies.test
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void TestLoginCorrectUser()
+        {
+            Userdb db = new Userdb();
+            LoginViewModel correctmodel = new LoginViewModel
+            {
+                Email = "noot@noot.noot",
+                Password = "nootnootnoot"
+            };
+            Assert.AreEqual(41,db.LoginUser(correctmodel),"Userlogin failed!");
+
+        }
+        [TestMethod]
+        public void TestLoginWrongEmail()
+        {
+            Userdb db = new Userdb();
+            LoginViewModel wrongemailmodel = new LoginViewModel
+            {
+                Email = "Noot@noooooot.noot",
+                Password = "nootnootnoot"
+            };
+            Assert.AreEqual(0, db.LoginUser(wrongemailmodel), "User logged in with fake email!");
+        }
+        [TestMethod]
+        public void TestLoginWrongPassword()
+        {
+            Userdb db = new Userdb();
+            LoginViewModel wrongpasswordmodel = new LoginViewModel
+            {
+                Email = "noot@noot.noot",
+                Password = "noooooooooot"
+            };
+            Assert.AreEqual(0, db.LoginUser(wrongpasswordmodel), "User logged in with wrong password!");
+
+        }
+
+        [TestMethod]
+        public void TestAdmin()
+        {
+            Userdb db = new Userdb();
+
+            Assert.AreEqual(true,db.IsAdmin(25),"Admin user not returned as admin!");
+        }
+
+        [TestMethod]
+        public void TestNotAdmin()
+        {
+            Userdb db = new Userdb();
+            Assert.AreEqual(false, db.IsAdmin(41), "Non admin user returned as adminuser!");
+
+        }
+    }
+}
